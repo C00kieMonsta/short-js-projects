cbefffd26f8010ed6f0f5551585e5a7f92dcc468
C00kieMonsta <Karagan.App@gmail.com>
2020-09-08 16:49:55 +0200

    revert export/import model which does not work with vanilla js

--- reviews/app.js
+++ reviews/app.js
@@ -1,4 +1,3 @@
-// local reviews data
 const reviews = [
   {
     id: 1,
@@ -37,3 +36,22 @@ const reviews = [
       "Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ",
   },
 ];
+
+
+// select DOM items
+const personImg = document.getElementById('person-img');
+const author = document.getElementById('author');
+const job = document.getElementById('job');
+const info = document.getElementById('info');
+
+const prevBtn= document.querySelector('.prev-btn');
+const nextBtn= document.querySelector('.next-btn');
+const randomBtn= document.querySelector('.random-btn');
+
+// starting item
+let currentItem = 0;
+
+// load initial item
+window.addEventListener('DOMContentLoaded', () => {
+  console.log('Loaded');
+});
