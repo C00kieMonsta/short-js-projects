d7429feb61cd7b63b6167c76cec1c0994baf9366
C00kieMonsta <Karagan.App@gmail.com>
2020-09-21 15:55:37 +0200

    fix navbab on scroll

--- scrolling/app.js
+++ scrolling/app.js
@@ -17,19 +17,38 @@ navToggle.addEventListener('click', () => {
     // we do not want to use hardcoded height
     // linksContainer.classList.toggle('show-links');
 
+    // adding inline style
     const containerHeight = linksContainer.getBoundingClientRect().height;
     const linksHeight = links.getBoundingClientRect().height;
-
     if (containerHeight === 0) {
         linksContainer.style.height = `${linksHeight}px`;
     } else {
         linksContainer.style.height = `${0}px`;
     }
-
-
 });
 
 // ********** fixed navbar ************
+const navbar = document.getElementById('nav');
+const topLink = document.querySelector('.top-link')
+
+window.addEventListener('scroll', () => {
+    const scrollHeigh = window.pageYOffset;
+    const navHeight = navbar.getBoundingClientRect().height;
+    
+    // fix navbar
+    if (scrollHeigh > navHeight) {
+        navbar.classList.add('fixed-nav');
+    } else {
+        navbar.classList.remove('fixed-nav');
+    }
+
+    // show top-link
+    if (scrollHeigh > 500) {
+        topLink.classList.add('show-link');
+    } else {
+        topLink.classList.remove('show-link');
+    }
+});
 
 // ********** smooth scroll ************
 // select links
