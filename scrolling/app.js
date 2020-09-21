b6812dfcaecfe48fa5d5b4de5cca11474d6de4e1
C00kieMonsta <Karagan.App@gmail.com>
2020-09-21 15:40:58 +0200

    dynamic toggle of menu

--- scrolling/app.js
+++ scrolling/app.js
@@ -8,6 +8,26 @@ const date = document.getElementById('date');
 date.innerHTML = new Date().getFullYear();
 
 // ********** close links ************
+const navToggle = document.querySelector('.nav-toggle');
+const linksContainer = document.querySelector('.links-container');
+const links = document.querySelector('.links');
+
+navToggle.addEventListener('click', () => {
+
+    // we do not want to use hardcoded height
+    // linksContainer.classList.toggle('show-links');
+
+    const containerHeight = linksContainer.getBoundingClientRect().height;
+    const linksHeight = links.getBoundingClientRect().height;
+
+    if (containerHeight === 0) {
+        linksContainer.style.height = `${linksHeight}px`;
+    } else {
+        linksContainer.style.height = `${0}px`;
+    }
+
+
+});
 
 // ********** fixed navbar ************
 
